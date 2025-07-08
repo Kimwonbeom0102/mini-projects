a9713a9d8ee3ae4dcfb250bc39b72783d9e38df0
Kimwonbeom0102 <kwb0102@gmail.com>
2025-07-08 17:03:28 +0900

    Stack 클래스 설계 프로젝트: 클래스 설계 및 테스트 완료

---
+++ practiceExcercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace practiceExcercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine($"Pop : {stack.Pop()}");
+            Console.WriteLine($"Pop : {stack.Pop()}");
+            Console.WriteLine($"Pop : {stack.Pop()}");
+
+            stack.Clear();
+        }
+    }
+}
---
+++ practiceExcercise2/Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceExcercise2
+{
+    public class Stack // LIFO 저장 자료구조
+    {
+        private List<object> list;
+        //public string name;
+
+        public Stack()
+        {
+            list = new List<object>(5);
+        }
+        public void Push(object obj) // 데이터를 넣는메서드
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException($"null은 스택에 추가할 수 없습니다. Null can't be addded in Stack .");
+            }
+            if (list.Count >= 5)
+            {
+                throw new InvalidOperationException(" 더 이상 넣을 수 없습니다. Stack is full.");
+            }
+
+            list.Add(obj);
+            Console.WriteLine($"obj 추가되었습니다. 마지막 obj : {obj}, 현재 count : {list.Count}. obj is added. Last obj : {obj}, Current count : {list.Count}");
+
+        }
+
+        public object Pop() // 데이터를 빼는 메서드 
+        {
+            if (list.Count == 0) // list = null 
+                throw new InvalidOperationException($"스택이 비었습니다. 현재 List : {list}. Stack is empty. Current Stack : {list}");
+
+                var lastIndex = list.Count - 1; // 스택 카운트 - 1 즉, 인덱스값 
+                object lastItem = list[lastIndex]; // 스택의 인덱스를 담아줌 즉, 요소(값)
+                list.RemoveAt(lastIndex); // 인덱스를 제거 ( 몇번째거 뺄지)
+                return lastItem; // 값 반환, 요소 
+
+        }
+        public void Clear() // 스택의 모든 요소를 제거하는 메서드 
+        {
+            list.Clear();
+            Console.WriteLine($"스택을 모두 비웠습니다. 현재 count : {list.Count}. Stack is cleared. Current count {list.Count}");
+        }
+    }
+}

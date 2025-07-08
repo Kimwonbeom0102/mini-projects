3bb3ad4a52362874b37779a0a5d5b2a4a55019b0
Kimwonbeom0102 <kwb0102@gmail.com>
2025-07-08 17:03:21 +0900

    Stopwatch 프로젝트: 스톱워치 기능 구현 완료

---
+++ practiceExcercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceExcercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            while (true)
+            {
+                Console.WriteLine("Anykey to start Stopwatch");
+
+                string s = Console.ReadLine();
+                if (s != null)
+                {
+                    stopwatch.Start();
+
+                    Console.WriteLine("Anykey to Stop in 3 seconds");
+                    Console.ReadLine();
+
+                    stopwatch.Stop();
+
+                    Console.WriteLine("Anykey to check Duration time");
+                    Console.ReadLine();
+                    Console.WriteLine(stopwatch.GetDuration());
+                    Console.WriteLine("Try again ? (y/n)");
+
+                    string s2 = Console.ReadLine();
+                    if (s2 != "y")
+                        break;
+                    else
+                        Console.Clear();
+
+                }
+            }
+        }
+    }
+}
---
+++ practiceExcercise/Stopwatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceExcercise
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime;  // 시작시간 
+        private DateTime _endTime;    // 끝 시간 
+        private bool _isRunning;      // 작동중인지 아닌지 < 작동중이면 다시 작동못하도록 bool타입 
+
+        public void Start()
+        {
+            if (_isRunning)  // 작동중이면 
+                throw new InvalidOperationException("Stopwatch is working");  // 이미 작동중으로 예외처리 
+
+            _startTime = DateTime.Now;
+            _isRunning = true;  // 작동중 
+        }
+
+        public void Stop()
+        {
+            if (!_isRunning)  // 작동중이 아니면 
+                throw new InvalidOperationException("Stopwatch doesn't work yet");  // 아직 작동하지 않았음 
+            _endTime = DateTime.Now;
+            _isRunning = false;
+        }
+
+        public TimeSpan GetDuration()
+        {
+            return _endTime - _startTime;  // 시간을 빼주고 return
+        }
+    }
+}

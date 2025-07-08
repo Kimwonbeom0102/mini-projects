d290a25e165c5a37ca945a5a216f7c066af69d1a
Kimwonbeom0102 <kwb0102@gmail.com>
2025-07-08 17:03:33 +0900

    StackOverflow 게시물 프로젝트: 기본 UI 및 동작 구현

---
+++ practiceExcercise3/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace practiceExcercise3
+{
+    internal class Program
+    {
+        internal void Main(string[] args)
+        {
+            while (true)
+            {
+                var post = new Post("Excercise ", "Intiallizing on Constructors");
+
+                Console.WriteLine(post.Title);  // 읽기, 쓰기 가능 
+                Console.WriteLine($"{post.Description} \n");  // 읽기만 가능 
+                post.Title = "Test - Change Title";
+                //stackOverflow.Description = "Try to change properties"; // 쓰기 실패
+                Console.WriteLine(post.Title); // 변경된 Title
+
+                Console.WriteLine("Choice Vote (up/down)");
+
+                while (true)
+                {
+                    string input = Console.ReadLine();
+
+                    if (input == "up")
+                    {
+                        post.Upvote();
+                    }
+                    else if (input == "down")
+                    {
+                        post.Downvote();
+                    }
+                    else if (input != "up" && input != "down")
+                        break;
+                }
+                Console.WriteLine($"Total VoteCount : {post.VoteCount}"); // Count 출력
+
+                Console.WriteLine("Try again ? (y/n)");
+
+                string s = Console.ReadLine();
+                if (s != "y")
+                    break;
+
+            }
+        }
+    }
+}
---
+++ practiceExcercise3/StackOverflow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceExcercise3
+{
+    public class Post
+    {
+        private int _voteCount;
+        public string Title {  get; set; }
+        public string Description {  get; private set; }
+        public DateTime CreatedDateTime { get; private set; }
+
+        public Post(string title, string description) // 객체 생성할 때 호출됨. 매개변수를 인자 받을 수 있음
+        {
+            _voteCount = 0;
+            Title = title;
+            Description = description; 
+            //Description = "Hi"; // 생성자 내부에서 변경가능
+            CreatedDateTime = DateTime.Now;
+        }
+
+        public void Upvote()
+        {
+            _voteCount++;
+        }
+
+        public void Downvote()
+        {
+            _voteCount--;
+        }
+
+        public int VoteCount 
+        {
+            get
+            {
+                return _voteCount;
+            }
+        }
+    }
+}

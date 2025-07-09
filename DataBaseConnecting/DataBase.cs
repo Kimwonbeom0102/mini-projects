a2ce02520768a80bafc70ac546246328b97a9343
Kimwonbeom0102 <kwb0102@gmail.com>
2025-07-09 22:12:06 +0900

    DataBaseConnecting 프로젝트 추가

---
+++ DataBaseConnecting/DataBase.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeExercise4
+{
+    // DbConnection 기반 클래스 (부모클래스)
+    // 두가지 속성을 가질 것
+    // 생성자를 생성해서 속성의 문자열을 전달받도록하고
+    // null 로 예외발생
+    // 2가지 메서드가 있지만 추상적임(자식클래스에서 확장)
+    // 해당 부모클래스에서 메서드는 추상메서드로 선언 
+    public abstract class DbConnection
+    {
+        public string ConnectionString { get; set; }
+        public TimeSpan Timeout { get; set; }
+
+        public DbConnection(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+
+            if (connectionString == null || string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentNullException(nameof(connectionString), "문자열이 null 이거나 비어있습니다.");
+
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentNullException(nameof(connectionString), "문자열이 null이거나 비어 있습니다.");
+
+            //throw new ArgumentNullException("빈 문자열입니다.");
+            
+            Timeout = TimeSpan.FromSeconds(10);
+
+        }
+
+        public abstract void Open(); // 여는 메서드 
+
+        public abstract void Close(); // 닫는메서드 
+    }
+
+    // 상속받는 클래스 구현 
+    // 자식 클래스는 생성했는데 속성은 따라오지만 생성자는 어떻게 해야하지 ?
+    //
+    public class SqlConnection : DbConnection
+    {
+        public SqlConnection(string connectionString) : base(connectionString) { }
+        
+        public override void Open()
+        {
+            Console.WriteLine("SQL 데이터베이스 연결을 열었습니다." + ConnectionString);
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine("SQL 데이터베이스 연결을 닫았습니다.");
+        }
+    }
+
+    public class OracleConnection : DbConnection
+    {
+        public OracleConnection(string connectionString) : base(connectionString) { }
+
+        public override void Open()
+        {
+            Console.WriteLine("Oracle 데이터베이스를 열었습니다." + ConnectionString);
+        }
+        public override void Close()
+        {
+            Console.WriteLine("Oracle 데이터베이스를 닫았습니다.");
+        }
+    }
+
+    
+}
+
+
---
+++ DataBaseConnecting/Program.cs
@@ -0,0 +1,57 @@
+﻿using PracticeExercise4;
+
+namespace PracticeExercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 타임아웃 속성이 있을 수 있다
+            // (예: 연결을 일정 시간 내에 열 수 없다면 예외가 발생한다)
+
+            DbConnection Conn = new SqlConnection("Server=myServer;Database=myDb;");
+            TimeSpan timeout = TimeSpan.FromSeconds(10);
+            Console.WriteLine($"데이터 베이스를 연결하시겠습니까? (y/n)?  {timeout.TotalSeconds}초 안에 눌러주세요");
+
+            // 이 부분에서 시간체크를 해야되는데 if 구문으로 하면 timeout은 int로 계산이 불가능하다네 ?
+            
+
+            string s1 = Console.ReadLine();
+            if(s1 == "y")
+            {
+                Console.WriteLine("데이터 베이스를 연결합니다. \n도중에 중단하려면 y키를 누르세요 \n");
+                string s2 = Console.ReadLine();
+                if (s2 == "y")
+                {
+                    Console.WriteLine("연결을 종료합니다.");
+                    Conn.Close();
+                    return;
+                }
+
+                Conn.Open();
+
+                Console.WriteLine("연결이 완료되었습니다. 연결을 닫으시려면 아무 키나 누르세요.\n");
+                
+                Console.ReadKey();
+                Console.WriteLine();
+                Conn.Close();
+
+                Console.WriteLine("창을 끄시려면 아무키나 눌러주세요\n");
+                Console.ReadKey();
+            }
+            else if(s1 == "n")
+            {
+                Conn.Close();
+
+                Console.WriteLine("창을 끄시려면 아무키나 누르세요");
+                Console.ReadKey();
+            }
+            Console.WriteLine(TimeSpan.FromSeconds(10));
+
+            // 데이터 베이스를 연결하시려면 y/n? + Timeout 실행 
+            // Timeout 시간 초과시 예외발생 
+            // y창 누르면 sqlConn.Open()
+            // n 창 누르면 exit sqlConn.Close() 실행
+        }
+    }
+}
